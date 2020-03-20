94ab387e99ce63075073696f2169e65cd3a73bff
pivanchev <pl.ivanchev@gmail.com>
2020-03-20 13:01:20 +0000

    initial commit

---
+++ src/com/pivanchev/MainApp.java
@@ -0,0 +1,21 @@
+package com.pivanchev;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		CustomEventPublisher cep = (CustomEventPublisher)context.getBean("customEventPublisher");
+		
+		cep.publish();
+		cep.publish();
+		
+		((ConfigurableApplicationContext)context).close();
+
+	}
+
+}
